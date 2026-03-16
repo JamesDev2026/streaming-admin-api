7b815f660d315c452a2c8dad87b07a03250098e1
JamesDev2026 <joiggre@gmail.com>
2026-03-15 20:43:14 -0400

    Adding the initial project

---
+++ src/main/java/com/streaming/admin/StreamingAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streaming/admin/StreamingAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
