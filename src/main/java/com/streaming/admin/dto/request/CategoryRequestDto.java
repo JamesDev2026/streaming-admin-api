faa461965ce99125103622ea95683d56f5ba39eb
JamesDev2026 <joiggre@gmail.com>
2026-07-09 02:01:43 -0400

    AllCategory - adding CRUD for category and SecurityConfig

---
+++ src/main/java/com/streaming/admin/dto/request/CategoryRequestDto.java
@@ -0,0 +1,22 @@
+package com.streaming.admin.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryRequestDto {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 45)
+    private String name;
+
+    @Size(max = 45)
+    private String description;
+}
